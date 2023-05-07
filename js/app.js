dbc9e5f2444ffbe3b8d79b4a67ec7217e4377933
glitton <glcodeworks@gmail.com>
2023-05-07 17:59:02 -0500

    home page and hero with diff layout and colors

--- js/app.js
+++ js/app.js
@@ -3,14 +3,7 @@ const navBtn = document.querySelector("#nav-btn");
 const closeBtn = document.querySelector("#close-btn");
 const sidebar = document.querySelector("#sidebar");
 const date = document.querySelector("#date");
-// add fixed class to navbar
-window.addEventListener("scroll", function () {
-  if (window.pageYOffset > 80) {
-    navbar.classList.add("navbar-fixed");
-  } else {
-    navbar.classList.remove("navbar-fixed");
-  }
-});
+
 // show sidebar
 navBtn.addEventListener("click", function () {
   sidebar.classList.add("show-sidebar");
@@ -20,3 +13,4 @@ closeBtn.addEventListener("click", function () {
 });
 // set year
 date.innerHTML = new Date().getFullYear();
+// console.log(date.innerHTML);
