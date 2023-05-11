98275f46b316f48e2ef2700ad1a9da2f7c5fd854
glitton <glcodeworks@gmail.com>
2023-05-11 14:46:18 -0500

    added left and rigth 0 to navbar-fixed class

--- js/app.js
+++ js/app.js
@@ -5,7 +5,7 @@ const sidebar = document.querySelector("#sidebar");
 // const date = document.querySelector("#date");
 // add fixed class to navbar
 window.addEventListener("scroll", function () {
-  if (window.pageYOffset > 20) {
+  if (window.pageYOffset > 5) {
     navbar.classList.add("navbar-fixed");
   } else {
     navbar.classList.remove("navbar-fixed");
