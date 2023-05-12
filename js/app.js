b6f0635468f3056c89ee5a65568687ba97617929
glitton <glcodeworks@gmail.com>
2023-05-12 17:18:20 -0500

    alternate pics set for mobile and big screen

--- js/app.js
+++ js/app.js
@@ -5,7 +5,7 @@ const sidebar = document.querySelector("#sidebar");
 const date = document.querySelector("#date");
 // add fixed class to navbar
 window.addEventListener("scroll", function () {
-  if (window.pageYOffset > 5) {
+  if (window.pageYOffset > 10) {
     navbar.classList.add("navbar-fixed");
   } else {
     navbar.classList.remove("navbar-fixed");
