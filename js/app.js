398e6e2bde163b1de27bb098f5808c910472d600
glitton <glcodeworks@gmail.com>
2023-05-18 20:59:21 -0500

    used window.pageYOffset instead of scrollY, set henav height using getBoundingClientRect().height

--- js/app.js
+++ js/app.js
@@ -3,10 +3,15 @@ const navBtn = document.querySelector("#nav-btn");
 const closeBtn = document.querySelector("#close-btn");
 const sidebar = document.querySelector("#sidebar");
 const date = document.querySelector("#date");
+
 // add fixed class to navbar
 window.addEventListener("scroll", function () {
-  // console.log(window.scrollY + "px");
-  if (window.scrollY > 40) {
+  const scrollHeight = window.pageYOffset;
+  // get height of navbar, make it fixed when scroll gets past this height
+  const navHeight = navbar.getBoundingClientRect().height;
+  // console.log(navHeight);
+
+  if (scrollHeight > navHeight) {
     navbar.classList.add("navbar-fixed");
   } else {
     navbar.classList.remove("navbar-fixed");
